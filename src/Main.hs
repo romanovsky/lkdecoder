3864edeb0da7ff9d26524e7e0d9827896b10e6f4
romanovsky <sergey@romanovsky.org>
2015-05-18 23:45:51 -0700

    almost there

--- src/Main.hs
+++ src/Main.hs
@@ -1,6 +1,7 @@
+{-# LANGUAGE OverlappingInstances #-}
 module Main where
 
-{-# LANGUAGE OverlappingInstances #-}
+
 
 import Prelude
 import System.IO
@@ -9,16 +10,15 @@ import Data.Attoparsec.Char8
 import Text.ParserCombinators.ReadP (many)
 import Control.Applicative ((<*))
 import Data.Either
-import qualified Data.ByteString as BC
---import qualified Data.ByteString.Char8 as BC
+import qualified Data.ByteString as BC-- (ByteString, unpack)
+import Data.Char (chr)
+--import Data.ByteString.Char8 (unpack)
 
 data Language = English | Russian deriving Eq
 
 initialLocale = Russian
 logFilePath :: FilePath
 logFilePath = "/home/serega/IdeaProjects/lkdecoder/log-sample.log"
-
-newtype LogMessage = LogMessage String deriving (Show)
 
 detectLanguage :: String -> Language
 detectLanguage s =
@@ -28,39 +28,54 @@ convertLine :: String -> String
 convertLine s =
     s
 
+data LogMessage = LogMessage String
+
 instance Show LogMessage where
     show ( LogMessage a ) = do
         if detectLanguage(a) == initialLocale
             then a
             else a
 
+{-# LANGUAGE OverlappingInstances #-}
+
 data LogEntry =
-    LogEntry { entryTime :: String
+    LogEntry { entryTime :: LocalTime
              , message :: LogMessage
-             } deriving Show
+             }
 
 
 type Log = [LogEntry]
 
-timeParser :: Parser LogEntry
+timeParser :: Parser LocalTime
 timeParser = do
-  stub  <- skipWhile (==" > ")
-  return $ LogEntry stub
+  stub  <- skipWhile (=='>')
+  let y = 1
+  let mm = 1
+  let d = 1
+  let h = 1
+  let m = 1
+  let s = 1
+  return $ LocalTime { localDay = fromGregorian (read y) (read mm) (read d)
+                     , localTimeOfDay = TimeOfDay (read h) (read m) (read s)
+                     }
 
-messageParser :: Parser LogEntry
-messageParser = do
+logMessageParser :: Parser LogMessage
+logMessageParser = do
     message <- takeByteString
-    return $ LogEntry message
+    return $ LogMessage $ map (chr . fromEnum) . BC.unpack $ message
 
 logEntryParser :: Parser LogEntry
 logEntryParser = do
     t <- timeParser
-    char " > "
-    m <- messageParser
+    char '>'
+    m <- logMessageParser
     return $ LogEntry t m
 
 logParser :: Parser Log
 logParser = many $ logEntryParser <* endOfLine
+--logParser = do
+--    let z =logEntryParser <* endOfLine
+--    return $ many z
 
 
 --main = do
@@ -71,11 +86,15 @@ logParser = many $ logEntryParser <* endOfLine
 --        putStrLn $ unlines output
 --        )
 
+--main = do
+--    let -- Parsed logs
+--        logs :: [Log]
+--        logs = rights $ fmap (parseOnly logParser) [logFilePath]
+--    BC.putStrLn $ logs
+
 main = do
-    let -- Parsed logs
-        logs :: [Log]
-        logs = rights $ fmap (parseOnly logParser) [logFilePath]
-    BC.putStrLn $ logs
+    putStrLn "a"
+
         -- Merged log
 --      mergedLog :: Log
 --      mergedLog = foldr merge [] logs
